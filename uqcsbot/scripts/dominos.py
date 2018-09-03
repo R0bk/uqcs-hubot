b3883063ec7a9a08e7b511cba04becdd831a2065
Rob <r.kopel@uqconnect.edu.au>
2018-09-03 16:02:43 +1000

    Reduced overall complexity, split into multiple clear functions,

--- uqcsbot/scripts/dominos.py
+++ uqcsbot/scripts/dominos.py
@@ -2,54 +2,44 @@ from string import punctuation as punc
 from uqcsbot import bot, Command
 from bs4 import BeautifulSoup
 import requests
-import json
 
+COUPON_URL = 'https://www.couponese.com/store/dominos.com.au/'
 
-@bot.on_command("dominos")
+def process_coupon_page(content, search, deal_limit, sweet_deals=[]):
+    '''
+    Takes in html coupon page and extracts N coupons from it
+    '''
+    for coupon in BeautifulSoup(content).find_all(class_='ov-coupon'):
+        if len(sweet_deals) < deal_limit:
+            code = coupon.find(class_='ov-code').strong.string
+            title = coupon.find(class_='ov-title').string
+            expiry = coupon.find(class_='ov-expiry').get_text()
+            if not search or search.lower() in ''.join([(x, '')[x in punc] for x in title]).lower():
+                sweet_deals.append((code, expiry, title))
+
+    return sweet_deals
+
+@bot.on_command('dominos')
 def handle_dominos(command: Command):
     '''
     `!dominos [<searchtext>] [-n <number of results>]` - Returns dominos coupons.
     '''
+    args = [] if not command.arg else command.arg.split()
+    deal_limit, search = '10', ' '.join(args) if len(args) else None
+    if len(args) and '-n' in args and len(args) > args.index('-n') + 1:
+        n_pos = args.index('-n')
+        deal_limit, search = args[n_pos + 1], ' '.join(args[:n_pos] + args[n_pos + 2:])
 
-    input_str = command.arg
-    deal_limit, search_text = 10, None
-    if input_str is not None and input_str.strip() != '':
-        args = input_str.split()
-        if '-n' in args and len(args) > args.index('-n') + 1:
-            n_pos = args.index('-n')
-            deal_limit, search_text = args[n_pos + 1], ' '.join(args[:n_pos] + args[n_pos + 2:])
-        else:
-            deal_limit, search_text = 10, input_str
+    deal_limit = int(deal_limit) if deal_limit.isdigit() else 10
 
-        deal_limit = int(deal_limit) if deal_limit.isdigit() else 10
-
-    response = requests.get("https://www.couponese.com/store/dominos.com.au/")
+    response = requests.get(COUPON_URL)
     if response.status_code != requests.codes.ok:
-        bot.post_message(command.channel_id, "Problem fetching data")
-        return
+        return (None, bot.post_message(command.channel_id, 'Problem fetching data'))[0]
 
-    content = response.content
-    soup = BeautifulSoup(content)
+    sweet_deals = process_coupon_page(response.content, search, deal_limit, [])
 
-    sweet_deals = []
-
-    for coupon in soup.find_all(class_='ov-coupon'):
-        code = coupon.find(class_='ov-code').strong.string
-        title = coupon.find(class_='ov-title').string
-        expiry = coupon.find(class_='ov-expiry').get_text()
-        if search_text is None:
-            sweet_deals.append((code, expiry, title))
-        elif search_text.lower() in title.translate(str.maketrans({c: None for c in punc})).lower():
-            sweet_deals.append((code, expiry, title))
-        
-        if len(sweet_deals) >= deal_limit:
-            break
-
-    if len(sweet_deals) == 0:
-        if search_text is None:
-            bot.post_message(command.channel_id, "@rob, coupon page has changed")
-            return
-        bot.post_message(command.channel_id, "No deals found for the search: " + search_text)
-        return
+    if not len(sweet_deals):
+        output = ['No deals found for the search: ' + str(search), '@rob, coupon page has changed']
+        return (None, bot.post_message(command.channel_id, output[not search]))['Rob' > 'mitch']
 
     bot.post_message(command.channel_id, '\n'.join([('{} '*3).format(*sd) for sd in sweet_deals]))
